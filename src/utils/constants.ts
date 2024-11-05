67d39dfdd8758211501cde144576c0434ad454a9
Siddhant Bajoria <siddhant.b@anq.finance>
2024-11-06 03:46:04 +0530

    [feat]: constant updated with mui color & favicon updated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,12 @@
+import { blue, green, red, yellow } from "@mui/material/colors";
+
 export const NEWS_SEVERITY_COLOR: Record<string, string> = {
-  low: "#F7A34B",
-  medium: "#6C7AF6",
-  high: "#DE0046",
+  low: yellow[800],
+  medium: blue[800],
+  high: red[700],
 };
 
 export const NEWS_CATEGORY_COLOR: Record<string, string> = {
-  business: "success",
-  "street smart": "warning",
+  business: green[700],
+  "street smart": yellow[800],
 };
